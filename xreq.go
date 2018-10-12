9f0eafbf40dbe6fbd6ff45cb1fb6cb48f4694d54
Leafney <babycoolzx@126.com>
2018-10-12 20:29:03 +0800

    zh to en

--- xreq.go
+++ xreq.go
@@ -5,6 +5,7 @@ req 扩展
 */
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"regexp"
@@ -12,37 +13,29 @@ import (
 	"time"
 )
 
-// *************************
+// *****Request********************
 
-// 自定义设置参数
+// Custom settings parameters
 type XSets map[string]interface{}
 
-// *************************
-
 // 请求基类
-func XRequest(url string, xSets map[string]interface{}, v ...interface{}) string {
+func XReq(url string, xSets map[string]interface{}, v ...interface{}) (r *Resp, err error) {
 
 	header := make(http.Header)
 
-	// 默认配置
+	// Default configuration
 
-	// 设置默认Content-Type
+	// Set default Content-Type
 	header.Set("Content-Type", "application/json;charset=utf-8")
 
-	// 设置默认User-Agent
+	// Set default User-Agent
 	header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36")
 
-	// 设置默认超时时间 默认10s
-	// req.SetTimeout(10 * time.Second)
-
-	// 请求类型
+	// Request Types
 	method_Type := "GET"
-	// 数据类型   default jsonp
-	data_Type := "default"
 
-	// 设置自定义配置参数
+	// Set custom configuration parameters
 	for key, value := range xSets {
-		// fmt.Println("key:", key, "value:", value)
 
 		ky := strings.ToLower(key)
 		switch ky {
@@ -53,68 +46,56 @@ func XRequest(url string, xSets map[string]interface{}, v ...interface{}) string
 		case "time-out":
 			SetTimeout(time.Duration(value.(int)) * time.Second)
 		case "proxy":
-			SetProxyUrl("http://my.proxy.com:23456")
+			SetProxyUrl(value.(string))
 		case "referer":
 			header.Set("Referer", value.(string))
 		case "debug":
 			Debug = value.(bool)
-		case "data-type":
-			// 请求类型
-			data_Type = strings.ToLower(value.(string))
 		case "method":
-			// 请求类型
+			// Request Types
 			method_Type = strings.ToUpper(value.(string))
 		default:
 			fmt.Sprintf("the %s-%s not default settings", key, value)
-			// 其他的参数，默认加入请求头中
+			// Other parameters are added to the request header by default.
 			header.Set(key, value.(string))
 		}
 	}
 
-	var r *Resp
-	var err error
-
 	// 根据请求类型，设置请求参数或请求体，发起请求
 	switch method_Type {
 	case "GET":
-		// 处理请求参数
-		fmt.Println("发起get请求")
-		// fmt.Println(v)
-		r, err = Get(url, v...)
+		return Get(url, header, v...)
 	case "POST":
-		r, err = Post(url, v...)
+		return Post(url, header, v...)
 	case "PUT":
-		r, err = Put(url, v...)
+		return Put(url, header, v...)
 	case "DELETE":
-		r, err = Delete(url, v...)
-
+		return Delete(url, header, v...)
+	default:
+		return
 	}
 
+}
+
+// *****Response********************
+
+// ToJSONfromJSONP convert jsonp response body string to json str and then to struct or map
+func (r *Resp) ToJSONfromJSONP(v interface{}) error {
+	data, err := r.ToString()
 	if err != nil {
-		fmt.Printf("[ERROR]request error: [%s] \n", err)
-		// Todo 请求出错后该如何处理
+		return err
 	}
 
-	fmt.Println("...*ResponseData*...")
-
-	// 判断StatusCode
-	resp := r.Response()
-	fmt.Println(resp.StatusCode)
-
-	// 返回结果--字符串
-	resp_str := r.String()
-	// 如果是 get 且 jsonp 则自动处理返回数据中的callback
-	if method_Type == "GET" && data_Type == "jsonp" {
-		pat := `^[^(]*?\((.*)\)[^)]*$` // 匹配 jQuery7955233(); 的jsonp格式
-		reg := regexp.MustCompile(pat)
-		resp := reg.FindStringSubmatch(resp_str)
-		if len(resp) == 2 {
-			resp_str = resp[1]
-		}
+	// Regular matching like `jQuery7955233 ();` jsonp format
+	pat := `^[^(]*?\((.*)\)[^)]*$`
+	reg := regexp.MustCompile(pat)
+	resp := reg.FindStringSubmatch(data)
+	if len(resp) == 2 {
+		data = resp[1]
 	}
 
-	fmt.Println(resp_str)
-	return resp_str
+	// Try JSON format parsing
+	return json.Unmarshal([]byte(data), v)
 }
 
 // *************************
--- xreq_test.go
+++ xreq_test.go
@@ -19,28 +19,28 @@ func TestX1(t *testing.T) {
 		"Debug":  true,
 		"Method": "GET",
 	}
-	str := XRequest(url, rs)
-	t.Log(str)
+	s, _ := XReq(url, rs)
+	t.Log(s)
 }
 
 // get请求 设置请求参数 发现无法为请求添加上参数
 // 通过比对 X2 和 X3的结果，后来发现 请求的req和设置参数的req是不同的req，所以才会赋不上值
-// 经过修改，主要是参数v ...interface{} 在Get() 中传参时是 `v...` 而不是 `v` ，这样就可以了
+// 经过修改，将参数v ...interface{} 在Get() 中传参时是 `v...` 而不是 `v` ，这样就可以了
 func TestX2(t *testing.T) {
 
 	// https://p.3.cn/prices/get?type=1&skuid=J_5338456
 	url := "https://p.3.cn/prices/get"
 	rs := XSets{
-		// "Content-Type": "application/javascript",
-		"Debug":  true,
-		"Method": "GET",
+		"Content-Type": "application/javascript",
+		"Debug":        true,
+		"Method":       "GET",
 	}
 	pa := Param{
 		"type":  "1",
 		"skuid": "J_5338456",
 	}
 
-	str := XRequest(url, rs, pa)
+	str, _ := XReq(url, rs, pa)
 	t.Log(str)
 }
 
